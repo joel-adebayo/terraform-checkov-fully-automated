ebfed9677d225bd375675fe1a3548aac9d835f4e
Joel Adebayo <jadebayo1308@gmail.com>
2023-12-19 17:35:59 -0600

    update key pair name

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = "1.4.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.62.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"  
 }
